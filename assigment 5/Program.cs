6d7c93d272ac740438de132aef6698c4bba89b0b
Nada Hamdy <nadaahasabbalh@gmail.com>
2026-03-02 13:01:11 +0200

    Add project files.

---
+++ assigment 5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace assigment_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
