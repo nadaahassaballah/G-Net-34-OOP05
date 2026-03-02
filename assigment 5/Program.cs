b885ef3283f07201f511acea4ab9c4bcf66cb3b4
Nada Hamdy <nadaahasabbalh@gmail.com>
2026-03-02 14:11:36 +0200

    q2

--- assigment 5/Program.cs
+++ assigment 5/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace assigment_5
+﻿using System.Diagnostics.Metrics;
+using System.IO;
+
+namespace assigment_5
 {
     internal class Program
     {
@@ -12,6 +15,16 @@
             //because we want to depend on abstraction, not implementation.
             //multiple implementations,loose coupling,better testability
             #endregion
+            #region q2
+            //            a) What is the problem with this design? Both interfaces have a method called Greet() — how does the class handle it currently?
+            // we need another great function and  the class translator has to implement both interfaces but it cant implement both great, it has to choose one of them and implement it
+            //b) How would you fix this so IEnglishSpeaker.Greet() says "Hello" and IArabicSpeaker.Greet() says "Ahlan"? What is this technique called?
+            //explicit interface implementation
+            //c) After applying your fix, can you call Greet() directly on a Translator object (e.g.translator.Greet())? Why or why not? How do you call each version?
+            // no because the method is implemented explicitly so we have to cast the obj , 
+
+            #endregion
+
             #endregion
         }
     }
