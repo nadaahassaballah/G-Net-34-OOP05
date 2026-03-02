b3dd2c4143e7bee81f77e074526d80322d9d520a
Nada Hamdy <nadaahasabbalh@gmail.com>
2026-03-02 14:00:43 +0200

    q1

--- assigment 5/Program.cs
+++ assigment 5/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region part 1
+            #region q1
+            //Q1 : What is an interface in C#? Why do we use interfaces instead of depending on concrete classes directly? Mention at least three benefits of using interfaces.
+
+            // interface is userdefine data type that its like a promise each segnitcher on it must implement it  and its like a contaract between the class and the interface;
+            //because we want to depend on abstraction, not implementation.
+            //multiple implementations,loose coupling,better testability
+            #endregion
+            #endregion
         }
     }
 }
